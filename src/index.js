130b791ed618c6c4327e5b0026993e593e59c8ba
Karl Mikkelsen <karl@karlmikko.com>
2015-11-20 15:55:23 +1100

    tests - linter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const mergeDucks = (...args) => 
+const mergeDucks = (...args) =>
   args.reduce((a = {}, b = {}) => {
     const initialState = {...a.initialState, ...b.initialState};
     const reducers = {...a.reducers, ...b.reducers};
     const actions = {...a.actions, ...b.actions};
     const reducer = (state = initialState, action) => {
-      const func = reducers[action.type] || (state => state);
-      return func(state, action);
+      const func = reducers[action.type];
+      return func && func(state, action) || state;
     };
     return {initialState, reducers, actions, reducer};
   }, {});
@@ -13,12 +13,12 @@ const mergeDucks = (...args) =>
 export default function TinyDuck(...args) {
   const ns = args[0];
 
-  if (typeof ns !== "string") {
-    return TinyDuck.apply(this, [""].concat(args));
+  if (typeof ns !== 'string') {
+    return TinyDuck.apply(this, [''].concat(args));
   }
 
   return args.splice(1).map(duck => Object.keys(duck).reduce((d, key) => {
-    if (key === "initialState") {
+    if (key === 'initialState') {
       return d;
     }
 
@@ -27,33 +27,40 @@ export default function TinyDuck(...args) {
 
     if (Object.prototype.toString.call(obj) === '[object Function]') {
       return mergeDucks(d, {
-        reducers: {[type]: obj}, 
-        actions: {[key]: type}
+        reducers: {[type]: obj},
+        actions: {[key]: type},
       });
     }
 
     // Poetically Duck Typed
     if (obj.reducer && obj.reducers && obj.actions && obj.initialState) {
+      const newNs = (act) => {
+      	if (act.substring(0, 1) === "/") {
+      		return act;
+      	}
+      	return `${type}/${act}`;
+      };
+
       const importReducers = Object.keys(obj.reducers).reduce((a, r) => ({
         ...a,
-        [`${type}/${r}`]: (state, action) => ({
-          ...state, 
-          [key]: obj.reducers[r](state[key], action)
-        })
+        [newNs(r)]: (state, action) => ({
+          ...state,
+          [key]: obj.reducers[r](state[key], action),
+        }),
       }), {});
 
       const importActions = Object.keys(obj.actions).reduce((a, r) => ({
         ...a,
-        [r]: `${type}/${obj.actions[r]}`
+        [r]: newNs(obj.actions[r]),
       }), {});
 
       return mergeDucks(d, {
-        initialState: {[key]: obj.initialState}, 
-        reducers: importReducers, 
-        actions: {[key]: importActions}
+        initialState: {[key]: obj.initialState},
+        reducers: importReducers,
+        actions: {[key]: importActions},
       });
     }
 
-    throw new Error("TinyDuck action handlers must be a function or a TinyDuck!");
+    throw new Error('TinyDuck action handlers must be a function or a TinyDuck!');
   }, {initialState: (duck.initialState || {})})).reduce(mergeDucks, {});
 }
--- test/tiny-duck.spec.js
+++ test/tiny-duck.spec.js
@@ -1,3 +1,96 @@
-import expect from 'expect'
-import TinyDuck from '../src/index'
+import expect from 'expect';
+import TinyDuck from '../src/index';
 
+const {reducer, actions, initialState} = TinyDuck('ns/', {
+  initialState: {test1:false},
+  ONE: (state, action) => ({...state, ...action, test1:true})
+}, {
+  sub: TinyDuck({
+    initialState: {test3: false},
+    ONE: (state, action) => ({...state, test3: true})
+  })
+}, {
+  sub2: TinyDuck("/",{
+    initialState: {test4: false},
+    ONE: (state, action) => ({...state, test4: true})
+  })
+}, {
+  initialState: {test2:false},
+  TWO: (state, action) => state
+});
+
+describe('composition', () => {
+  it('merge initialState with nesting preserved', () => {
+    expect(initialState).toEqual({
+      test1: false, 
+      test2: false, 
+      sub: {test3: false}, 
+      sub2: {test4: false}
+    });
+  });
+  it('merge actions with nesting preserved + sub namespace', () => {
+    expect(actions).toEqual({
+      ONE: 'ns/ONE', 
+      sub: {ONE: 'ns/sub/ONE'}, 
+      sub2: {ONE: '/ONE'}, 
+      TWO: 'ns/TWO'
+    });
+  });
+});
+
+describe('dispatch', () => {
+  const history = [];
+
+  const dispatch = (action, payload) => {
+    history.push(reducer(history.slice(-1)[0], {
+      ...payload,
+      type: action
+    }));
+  };
+
+  dispatch(actions.ONE, {wow:true});
+  dispatch(actions.ONE, {wow:false});
+  dispatch(actions.sub.ONE, {});
+  dispatch(actions.sub2.ONE, {});
+  dispatch(actions.TWO, null);
+  dispatch(null, null);
+
+  it('reducer processes all actions', () => {
+    expect(history).toEqual([ { test1: true,
+    sub: { test3: false },
+    sub2: { test4: false },
+    test2: false,
+    wow: true,
+    type: 'ns/ONE' },
+  { test1: true,
+    sub: { test3: false },
+    sub2: { test4: false },
+    test2: false,
+    wow: false,
+    type: 'ns/ONE' },
+  { test1: true,
+    sub: { test3: true },
+    sub2: { test4: false },
+    test2: false,
+    wow: false,
+    type: 'ns/ONE' },
+  { test1: true,
+    sub: { test3: true },
+    sub2: { test4: true },
+    test2: false,
+    wow: false,
+    type: 'ns/ONE' },
+  { test1: true,
+    sub: { test3: true },
+    sub2: { test4: true },
+    test2: false,
+    wow: false,
+    type: 'ns/ONE' } ,
+  { test1: true,
+    sub: { test3: true },
+    sub2: { test4: true },
+    test2: false,
+    wow: false,
+    type: 'ns/ONE' } ]);
+  });
+});
